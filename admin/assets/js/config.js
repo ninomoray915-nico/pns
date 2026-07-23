a651c6caee6cd149ec412a88e8d2e21935d35090
ninomoray915-nico <ninomoray915@gmail.com>
2026-07-23 12:38:36 +0800

    Update config.js

--- admin/assets/js/config.js
+++ admin/assets/js/config.js
@@ -1,4 +1,4 @@
 window.NINO_CONFIG={
-  SUPABASE_URL:"https://iskbjlohnhzjodpzwnw.supabase.co",
+  SUPABASE_URL:"https://iskbjlohnhzjodpzwznw.supabase.co",
   SUPABASE_PUBLISHABLE_KEY:"sb_publishable_BEvX6Gr5eRoFGVYtX-a76A_2ko8B-k6"
 };
